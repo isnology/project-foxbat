526ecf2aa7600a31a9436a4fc8e7bb152838ff27
NathanielMether <nmet3@student.monash.edu>
2018-01-16 10:37:05 +1100

    Added Save form component

--- web/src/App.js
+++ web/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from './components/Button';
 import PlaneSelect from './components/PlaneSelect';
+import SaveForm from './components/SaveForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SaveForm/>
       </div>
     );
   }
