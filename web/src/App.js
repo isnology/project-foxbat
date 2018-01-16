a4e0760145201be0d1c50da6ddc727dfa2a842b4
NathanielMether <nmet3@student.monash.edu>
2018-01-16 13:45:17 +1100

    Adding panel template for user landing page

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,7 @@ import Button from './components/Button';
 import Sidebar from './components/sidebar/Sidebar';
 import PlaneSelect from './components/PlaneSelect';
 import Form from './components/Form';
+import PanelTemplate from './components/PanelTemplate';
 
 class App extends Component {
   render() {
