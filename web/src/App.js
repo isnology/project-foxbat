41f6880ca532bf2f5c60f9ca9560912a17e72b6b
NathanielMether <nmet3@student.monash.edu>
2018-01-16 13:29:50 +1100

    Revert "Revert "Added Panel Template for the user landing page""
    This reverts commit a09dd1f10276bfe1e7fd5833fa8e806df38f6cbf.

--- web/src/App.js
+++ web/src/App.js
@@ -4,23 +4,12 @@ import Button from './components/Button';
 import Sidebar from './components/sidebar/Sidebar';
 import PlaneSelect from './components/PlaneSelect';
 import Form from './components/Form';
+import PanelTemplate from './components/PanelTemplate';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Form text="Please enter your email address to save
-          and give your panel a name. The url
-          for your dashboard will be sent to
-          your email address."
-          FirstInputLabel="Panel Name"
-          FirstInputType="text"
-          SecondInputLabel="Email"
-          SecondInputType="text"
-          ThirdInputLabel="Password"
-          ThirdInputType="password"
-          buttonText="save"
-        />
       </div>
     );
   }
