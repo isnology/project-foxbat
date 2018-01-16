8d1fc2734c3e7f6a42d00d3ac1294012c83ce503
IsNology <glenn@isnology.com>
2018-01-16 16:09:41 +1100

    add token.js and signup, signin and signout api's to app.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { signIn, signUp, signOutNow } from './api/auth'
+import { getDecodedToken } from './api/token'
 import './App.css';
 import Button from './components/Button';
 import Sidebar from './components/sidebar/Sidebar';
@@ -8,7 +10,32 @@ import PanelTemplate from './components/PanelTemplate';
 
 class App extends Component {
   state = {
+    decodedToken: getDecodedToken(), // Restore the previous signed in data
+  }
 
+  onSignIn = ({ email, password }) => {
+    signIn({ email, password })
+    .then((decodedToken) => {
+      this.setState({ decodedToken })
+    })
+    .catch((error) => {
+      this.setState({ error })
+    })
+  }
+
+  onSignUp = ({ email, password }) => {
+    signUp({ email, password })
+    .then((decodedToken) => {
+      this.setState({ decodedToken })
+    })
+    .catch((error) => {
+      this.setState({ error })
+    })
+  }
+
+  onSignOut = () => {
+    signOutNow()
+    this.setState({ decodedToken: null })
   }
 
   render() {
