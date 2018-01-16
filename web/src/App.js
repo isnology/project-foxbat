581e366b4244f9e7ef021402f062214f1a31e0c6
NathanielMether <nmet3@student.monash.edu>
2018-01-16 15:43:46 +1100

    Added landing page route

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import Button from './components/Button';
 import Sidebar from './components/sidebar/Sidebar';
 import PlaneSelect from './components/PlaneSelect';
@@ -9,9 +10,40 @@ import PanelTemplate from './components/PanelTemplate';
 class App extends Component {
   render() {
     return (
+<<<<<<< Updated upstream
       <div className="App">
                 
       </div>
+=======
+      <Router>
+        <div className="App">
+          <Switch>
+
+            <Route path='/' exact render={ () => (
+              <Fragment>
+                <h1>Welcome to the Foxbat Instrument Panel Configurator</h1>
+                <br/>
+                <h2>Which plane are you configuring for?</h2>
+                <br/>
+
+                <PlaneSelect name="A32 Vixxen" imageURL=""/>
+                <PlaneSelect name="A22 Foxbat/Kelpie"/>
+
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <div>
+                  <Button text="Lost your panel URL?"/>
+                </div>
+              </Fragment>
+            )}/>
+
+          </Switch>
+        </div>
+      </Router>
+>>>>>>> Stashed changes
     );
   }
 }
--- web/src/components/Form.js
+++ web/src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import RoundExitButton from './RoundExitButton';
 import Button from './Button';
 
