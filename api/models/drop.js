0f6dc17a4e1e68f0e5e7761821d227ced996d2f5
Alex Palma <alex-palma@outlook.com>
2018-01-21 01:26:47 +1100

    Prevent database cleanup tasks from exiting prematurely

--- api/models/drop.js
+++ api/models/drop.js
@@ -6,55 +6,51 @@ const InstrumentClass = require('./InstrumentClass')
 const User = require('./User')
 
 
-//Panel.deleteMany()
-//.then(() => {
-//  console.log('Deleted panels')
-//  process.exit()
-//})
+Panel.deleteMany()
+.then(() => {
+ console.log('Deleted panels')
+})
 
-//Template.deleteMany()
-//.then(() => {
-//  console.log('Deleted templates')
-//  process.exit()
-//})
+Template.deleteMany()
+.then(() => {
+ console.log('Deleted templates')
+})
 
-//Instrument.deleteMany()
-//.then(() => {
-//  console.log('Deleted instruments')
-//  process.exit()
-//})
+Instrument.deleteMany()
+.then(() => {
+ console.log('Deleted instruments')
+})
 
-//InstrumentClass.deleteMany()
-//.then(() => {
-//  console.log('Deleted users')
-//  process.exit()
-//})
+InstrumentClass.deleteMany()
+.then(() => {
+ console.log('Deleted users')
+})
 
 
-//User.deleteMany()
-//.then(() => {
-//  console.log('Deleted users')
-//  process.exit()
-//})
+User.deleteMany()
+.then(() => {
+ console.log('Deleted users')
+ process.exit()
+})
 
-mongoose.connection.collections['Panel'].drop( function(err) {
-  console.log('Panel dropped');
-});
+// mongoose.connection.collections['Panel'].drop( function(err) {
+//   console.log('Panel dropped');
+// });
 
-mongoose.connection.collections['Template'].drop( function(err) {
-  console.log('Template dropped');
-});
+// mongoose.connection.collections['Template'].drop( function(err) {
+//   console.log('Template dropped');
+// });
 
-mongoose.connection.collections['Instrument'].drop( function(err) {
-  console.log('Instrument dropped');
-});
+// mongoose.connection.collections['Instrument'].drop( function(err) {
+//   console.log('Instrument dropped');
+// });
 
-mongoose.connection.collections['InstrumentClass'].drop( function(err) {
-  console.log('InstrumentClass dropped');
-});
+// mongoose.connection.collections['InstrumentClass'].drop( function(err) {
+//   console.log('InstrumentClass dropped');
+// });
 
-mongoose.connection.collections['User'].drop( function(err) {
-  console.log('User dropped');
-});
+// mongoose.connection.collections['User'].drop( function(err) {
+//   console.log('User dropped');
+// });
 
 
