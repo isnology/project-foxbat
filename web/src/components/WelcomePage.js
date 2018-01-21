0eaca46941519d05ed00717740a142f8c2839a89
piratechicken <simonadwyer@yahoo.com.au>
2018-01-22 10:43:18 +1100

    images

--- web/src/components/WelcomePage.js
+++ web/src/components/WelcomePage.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import Button from './Button'
 import PlaneSelect from './PlaneSelect'
+import a22pic from '../img/a22.jpg'
+import a32pic from '../img/a32.jpg'
 
 function WelcomePage({ onSignOut, doModalWindow, signedIn }) {
   return (
@@ -13,12 +15,13 @@ function WelcomePage({ onSignOut, doModalWindow, signedIn }) {
 
       <Link to="/a32">
         <PlaneSelect
-        name="A32 Vixxen" imageURL="http://www.foxbat.com.au/public/editor_images/A32_04.jpg"/>
+        name="A32 Vixxen" 
+        imageURL={ a32pic }/>
       </Link>
       <Link to="/a22">
         <PlaneSelect
         name="A22 Foxbat/Kelpie"
-        imageURL="http://www.foxbat.com.au/public/editor_images/A22LS_yellow_01.jpg"/>
+        imageURL={ a22pic }/>
       </Link>
 
       <br/>
